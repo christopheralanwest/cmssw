e6f43e8dad15c91774d7d1fb5bcf53bbbf8f117a
Chris West <christopher.alan.west@gmail.com>
2019-10-29 22:36:04 +0100

    Add post-fix to allow for labeled JEC for fast simulation

--- PhysicsTools/PatAlgos/python/recoLayer0/jetCorrections_cff.py
+++ PhysicsTools/PatAlgos/python/recoLayer0/jetCorrections_cff.py
@@ -3,6 +3,11 @@ import FWCore.ParameterSet.Config as cms
 from PhysicsTools.PatAlgos.recoLayer0.jetCorrFactors_cfi import *
 from JetMETCorrections.Configuration.JetCorrectionServicesAllAlgos_cff import *
 
+from Configuration.Eras.Modifier_fastSim_cff import fastSim
+
+fastSimPostFix = '_FS'
+fastSim.toModify(patJetCorrFactors, payload=cms.string(patJetCorrFactors.payload.value() + fastSimPostFix))
+
 ## for scheduled mode
 patJetCorrectionsTask = cms.Task(patJetCorrFactors)
 patJetCorrections = cms.Sequence(patJetCorrectionsTask)
